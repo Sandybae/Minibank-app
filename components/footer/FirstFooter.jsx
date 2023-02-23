31235788f38ef09d6cd63228fdc6519e5e2dd8c3
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-23 16:18:29 +0000

    style firstfooter background

--- components/footer/FirstFooter.jsx
+++ components/footer/FirstFooter.jsx
@@ -3,7 +3,7 @@ import { FaCaretRight } from "react-icons/fa";
 
 const FirstFooter = () => {
     return ( 
-        <div className="grid bg-cyan-900 text-base text-green-100 px-20 py-10">
+        <div className="grid bg-slate-700 text-base text-green-100 px-20 py-10">
             <h1 className="py-5">Ecobank Banking</h1>
             <div className="grid grid-cols-2">
                 <div className="flex px-15 py-2"><FaCaretRight className="mt-1"/><Link href="#">Consumer Banking</Link></div>
