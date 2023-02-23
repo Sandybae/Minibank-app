6f205e9cb38f37544219556f5fa828eb08068da6
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-23 17:03:58 +0000

    added images to content

--- components/MainPage/SixthPage.jsx
+++ components/MainPage/SixthPage.jsx
@@ -3,14 +3,14 @@ import { FaCheck } from "react-icons/fa";
 const SixthPage = () => {
 
     return ( 
-       <div className="flex justify-around text-center  hover:bg-gray-800 hover:text-white">
-          <div className="w-1/2 hover:bg-gray-600 text-left pl-20">
+       <div className="flex justify-around text-center  hover:bg-gray-600 hover:text-white">
+          <div className="w-1/2 text-left pl-20">
             <p className="text-4xl text-green-400 my-5">Diaspora Current Account</p>
             <p className="font-bold text-4xl my-10">Manage and move your money more easily while living and working away from home</p>
             <button className="bg-green-900 text-white text-sm text-bold rounded hover:border hover:border-white p-3 mx-2">APPLY NOW</button>
             <button className="bg-green-900 text-white text-sm rounded text-bold hover:border hover:border-white p-3 mx-2">DOWNLOAD PDF FORM</button>
           </div>
-          <div className="w-1/2 py-10 pl-20 hover:bg-gray-600">
+          <div className="w-1/2 py-10 pl-20 hover:bg-[url('./images/diaspora.jpeg')]">
              <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>Debit Card</div>
              <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>Internet Banking</div>
              <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>Mobile Banking</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./lib/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{jpg,jpeg,webp}",
   ],
   theme: {
     extend: {},
