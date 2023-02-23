b3eb476cc9f2e2885ca93cd6391032e785a0cd67
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-23 16:11:55 +0000

    structured the secondfooter.

---
+++ components/footer/SecondFooter.jsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import { FaRegComments, FaYoutube, FaFacebookF, FaTwitter, FaInstagram, FaLinkedin, FaRegCopyright, FaSearch } from 'react-icons/fa';
+
+const SecondFooter = () => {
+    return ( 
+        <div className='bg-slate-900 p-4'>
+            <div className='flex justify-around border-b border-gray-400 py-7'>
+                <div className='flex'>
+                    <input
+                       className='border rounded-l-sm'
+                       type="text"
+                       placeholder='SEARCH'
+                    />
+                    <div className='bg-lime-400 text-white p-4 rounded-r-sm'>
+                    <Link href="#"><FaSearch/></Link>
+                    </div>
+                </div>
+                <div className='flex gap-5 text-white text-2xl'>
+                    <a href='https://facebook.com/live-chat'><FaRegComments/></a>
+                    <a href='https://youtube.com'><FaYoutube/></a>
+                    <a href='https://facebook.com'><FaFacebookF/></a>
+                    <a href='https://twitter.com'><FaTwitter/></a>
+                    <a href='https://instagram.com'><FaInstagram/></a>
+                    <a href='https://linkedin.com'><FaLinkedin/></a>
+                </div>
+            </div>
+            <div className='flex justify-end items-center text-blue-400 p-3 gap-2'>
+                <FaRegCopyright/>
+                <p>MINIBANK 2023</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default SecondFooter;
--- pages/Footer.js
+++ pages/Footer.js
@@ -1,9 +1,11 @@
 import FirstFooter from "../components/footer/FirstFooter";
+import SecondFooter from "../components/footer/SecondFooter";
 
 const Footer = () => {
   return (
     <div>
       <FirstFooter />
+      <SecondFooter />
     </div>
   );
 };
