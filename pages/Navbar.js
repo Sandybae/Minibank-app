349ec729fc85b67d5e7a1019d3f5fea126801105
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-23 17:40:10 +0000

    restyled the navbar

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -6,8 +6,8 @@ import BankingServices from "../components/navbar/BankingServices";
 
 const Navbar = () => {
   return (
-    <nav className="">
-      <div className="flex justify-around p-2 text-s text-white bg-blue-400">
+    <nav>
+      <div className="flex justify-around p-2 text-s text-gray-900 bg-violet-200">
         <div>
           <BankingType />
         </div>
@@ -16,7 +16,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="flex justify-around bg-blue-800 p-8 text-white border-b border-gray-500">
+      <div className="flex justify-around bg-violet-900 p-8 text-gray-200 border-b border-gray-500">
         <div>
           <Link className="font-extrabold text-4xl" href="#">
             MINIBANK
