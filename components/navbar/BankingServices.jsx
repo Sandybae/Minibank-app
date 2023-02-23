0b9859cd9c035a721278029b01c1105af28c9244
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-23 12:53:02 +0000

    restructured the bankingservice file.

--- components/navbar/BankingServices.jsx
+++ components/navbar/BankingServices.jsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import { FaHome, FaAngleDown } from "react-icons/fa";
 
-const servicesAvailable = [
-    {name: 'Everyday Banking', icon: FaAngleDown },
-    {name: 'Payments and Transfers', icon: FaAngleDown },
-    {name: 'Ways To Bank', icon: FaAngleDown },
-    {name: 'Product and Services', icon: FaAngleDown },
-    {name: 'Help and Security', icon: FaAngleDown }
-]
 
 const BankingServices = () => {
-    return ( 
+  
+  const servicesAvailable = [
+      {name: 'Everyday Banking', icon: FaAngleDown },
+      {name: 'Payments and Transfers', icon: FaAngleDown },
+      {name: 'Ways To Bank', icon: FaAngleDown },
+      {name: 'Product and Services', icon: FaAngleDown },
+      {name: 'Help and Security', icon: FaAngleDown }
+  ]
+  return ( 
         <div className="flex gap-20 items-center bg-blue-800 text-white p-2 text-base">
           <Link href="/" className="px-10  hover:bg-white hover:text-blue-800 p-3"><FaHome/></Link>
           { servicesAvailable.map(list => {
