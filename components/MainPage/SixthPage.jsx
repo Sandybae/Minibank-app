7e21eef8de5713a4f0afcca1553a9e0473b2c6be
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-23 17:16:49 +0000

    styled sixthpage.

--- components/MainPage/SixthPage.jsx
+++ components/MainPage/SixthPage.jsx
@@ -5,19 +5,19 @@ const SixthPage = () => {
     return ( 
        <div className="flex justify-around text-center  hover:bg-gray-600 hover:text-white">
           <div className="w-1/2 text-left pl-20">
-            <p className="text-4xl text-green-400 my-5">Diaspora Current Account</p>
-            <p className="font-bold text-4xl my-10">Manage and move your money more easily while living and working away from home</p>
-            <button className="bg-green-900 text-white text-sm text-bold rounded hover:border hover:border-white p-3 mx-2">APPLY NOW</button>
-            <button className="bg-green-900 text-white text-sm rounded text-bold hover:border hover:border-white p-3 mx-2">DOWNLOAD PDF FORM</button>
+            <p className="text-4xl text-gray-900 my-5">Diaspora Current Account</p>
+            <p className="font-bold text-4xl my-10 w-3/5">Manage and move your money more easily while living and working away from home</p>
+            <button className="bg-gray-700 text-white text-sm text-bold rounded hover:border hover:border-white p-3 mx-2">APPLY NOW</button>
+            <button className="bg-gray-700 text-white text-sm rounded text-bold hover:border hover:border-white p-3 mx-2">DOWNLOAD PDF FORM</button>
           </div>
-          <div className="w-1/2 py-10 pl-20 hover:bg-[url('./images/diaspora.jpeg')]">
-             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>Debit Card</div>
-             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>Internet Banking</div>
-             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>Mobile Banking</div>
-             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>Standing Orders</div>
-             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>Relationship Manager</div>
-             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>International transfers</div>
-             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-400"/>Access to your money in 33 countries</div>
+          <div className="w-1/2 py-10 pl-20">
+             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-300"/>Debit Card</div>
+             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-300"/>Internet Banking</div>
+             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-300"/>Mobile Banking</div>
+             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-300"/>Standing Orders</div>
+             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-300"/>Relationship Manager</div>
+             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-300"/>International transfers</div>
+             <div className="flex text-xl mx-5 py-5"><FaCheck className="mr-5 text-blue-300"/>Access to your money in 33 countries</div>
           </div>
        </div>
    );
