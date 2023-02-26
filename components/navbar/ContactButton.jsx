a544e649eedd4b061ef3af814be2c8a43531ed77
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-26 13:42:46 +0000

    defined the contact button

--- components/navbar/ContactButton.jsx
+++ components/navbar/ContactButton.jsx
@@ -3,13 +3,13 @@ import { FaPhone } from 'react-icons/fa';
 const ContactButton = () => {
     return ( 
         <div>
-            <div>
+            <div className='flex justify-start text-gray-900 m-3'>
                 <FaPhone/>
                 <p>Contact</p>
             </div>
             <div>
-                <button>Get in Touch</button>
-                <button>Find a service</button>
+                <button className='bg-violet-800 text-white border-b border-black m-3 p-4'>Get in Touch</button>
+                <button className='bg-violet-800 text-white border-b border-black m-3 p-4'>Find a service</button>
             </div>
         </div>
      );
--- components/navbar/SmallScreen.jsx
+++ components/navbar/SmallScreen.jsx
@@ -1,16 +1,22 @@
+import MenuButton from "./MenuButton";
+import ContactButton from "./ContactButton";
+import Link from "next/link";
+import { FaLock, FaPhone, FaBars} from 'react-icons/fa';
+import { useState } from "react";
+
 const SmallScreen = () => {
 
     const toggle = () => {
           setOpen(!open);
         }
-
+    const [open, setOpen] = useState(false);
     return ( 
       <div className="flex items-right gap-5 mr-10 text-lg">
         <Link href="#">
           <FaLock className="ml-3" />
           <p>LogIn</p>
         </Link>
-        <Link href="#">
+        <Link href="#" onClick={toggle}>
           <FaPhone className="ml-3" />
           <p>Contact</p>
         </Link>
@@ -21,6 +27,7 @@ const SmallScreen = () => {
           </Link>
         </div>
         {open && <MenuButton />}
+        {open && <ContactButton />}
       </div>
      );
 }
