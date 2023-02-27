e0cfebe1e80f9dba986184ee3bc86efc38f636f4
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-27 14:00:25 +0000

    restyling the applypage

--- pages/applynow.jsx
+++ pages/applynow.jsx
@@ -4,9 +4,16 @@ import Link from 'next/link';
 
 const ApplyNow = () => {
     return ( 
-        <div className='bg-url()'>
-            <div>
-                <h1>MINIBANK</h1>
+        <div className='bg-[url()]'>
+            <div className='dropdown'></div>
+            <div className=''>
+                <div className='flex justify-between'>
+                    <h1>MINIBANK</h1>
+                    <div>
+                        <p>Having Troubles?</p>
+                        <Link href="#">Get Help</Link>
+                    </div>
+                </div>
                 <div>
                     <Link href="#">Current account</Link>
                     <div>
