39fe76d7663584d196e6040d8751ff2838db0ee6
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-27 16:29:16 +0000

    renamed files

---
+++ pages/footer.jsx
@@ -0,0 +1,13 @@
+import FirstFooter from '../components/footer/FirstFooter';
+import SecondFooter from '../components/footer/SecondFooter';
+
+function Footer() {
+  return (
+    <div>
+      <FirstFooter />
+      <SecondFooter />
+    </div>
+  );
+}
+
+export default Footer;
