2afda3bcdc10e30512d388d5ff2b773b51dcf280
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-28 18:20:30 +0000

    restyled applynow button

--- pages/applynow.jsx
+++ pages/applynow.jsx
@@ -1,41 +1,20 @@
 import React from 'react';
-import { FaCheckCircle } from 'react-icons/fa';
-import Link from 'next/link';
+import Applyhead from '../components/Applynow/Applyhead';
+import Applybody from '../components/Applynow/Applybody';
 
 function ApplyNow() {
   return (
-    <div className="bg-[url()]">
-      <div className="dropdown" />
-      <div className="">
-        <div className="flex justify-between">
-          <h1>MINIBANK</h1>
-          <div>
-            <p>Having Troubles?</p>
-            <Link href="#a">Get Help</Link>
-          </div>
-        </div>
-        <div>
-          <Link href="#a">Current account</Link>
-          <div>
-            <FaCheckCircle />
-            <p>Debit Card</p>
-          </div>
-          <div>
-            <FaCheckCircle />
-            <p>Minibank Online</p>
-          </div>
-          <div>
-            <FaCheckCircle />
-            <p>Mobile Banking</p>
-          </div>
-          <div>
-            <FaCheckCircle />
-            <p>Standing Orders</p>
-          </div>
-          <Link href="#a">Savings account</Link>
-        </div>
+    <div>
+
+      <div className="bg-cover bg-[url('/backgroundimg-1.jpg')] h-[80svh] relative " />
+      <div className="w-full max-w-4xl absolute top-24 left-24  border-0 rounded-md shadow-md py-3 px-8 bg-gradient-to-r from-gray-200 to-gray-400">
+        <div><Applyhead /></div>
+        <div><Applybody /></div>
       </div>
-      <div />
+      <div className="flex flex-col">
+        <h1 className="text-violet-500 text-3xl font-bold">Personal Load</h1>
+        <p className="text-gray-500 text-xl">Ready to go wherever you want to go ...</p>
+      </div>
     </div>
   );
 }
